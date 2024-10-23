2810e5b2872613dc4b6b4b800d0777c7e7ce1656
stonedeva <leonardstonehoff@gmail.com>
2024-10-23 19:35:59 +0200

    Add basic testing system

--- output.asm
+++ output.asm
@@ -37,143 +37,54 @@ main:
 	mov byte [call_flag], 0
 	jne block_addr_0
 addr_1:
-	mov rax, 0
+	mov rax, 500
 	push rax
-	pop [index]
 addr_2:
-	mov rax, 0
+	mov rax, 500
 	push rax
-	pop [result]
-loopaddr_0:
-addr_4:
-	mov rax, [index]
-	push rax
-addr_5:
-	mov rax, 1000
-	push rax
-addr_6:
+addr_3:
 	pop rax
 	pop rbx
 	cmp rax, rbx
-	setg al
+	sete al
 	movzx rax, al
         push rax
-addr_7:
+addr_4:
 	pop rax
 	mov [cond_flag], al
 	cmp rax, 1
-	je addr_8
-	jne endloop_addr_0
-addr_8:
-	mov rax, 3
+	je addr_5
+	jne endif_addr_0
+addr_5:
+	mov rax, 99
 	push rax
+addr_6:
+	pop rdi
+	call dump
+endif_addr_0:
+	cmp [cond_flag], 1
+	je endif_addr_1
+addr_7:
+	mov rax, 205
+	push rax
+addr_8:
+	pop rdi
+	call dump
+endif_addr_1:
 addr_9:
-	mov rax, [index]
+	mov rax, 0
 	push rax
 addr_10:
 	pop rax
-	pop rbx
-	xor rdx, rdx
-	div rbx
-	mov rax, rdx
-        push rax
-addr_11:
-	mov rax, 0
-	push rax
-addr_12:
-	pop rax
-	pop rbx
-	cmp rax, rbx
-	sete al
-	movzx rax, al
-        push rax
-addr_13:
-	mov rax, 5
-	push rax
-addr_14:
-	mov rax, [index]
-	push rax
-addr_15:
-	pop rax
-	pop rbx
-	xor rdx, rdx
-	div rbx
-	mov rax, rdx
-        push rax
-addr_16:
-	mov rax, 0
-	push rax
-addr_17:
-	pop rax
-	pop rbx
-	cmp rax, rbx
-	sete al
-	movzx rax, al
-        push rax
-addr_18:
-	pop rax
-	pop rbx
-	or rax, rbx
-        push rax
-addr_19:
-	pop rax
-	mov [cond_flag], al
-	cmp rax, 1
-	je addr_20
-	jne endif_addr_0
-addr_20:
-	mov rax, [result]
-	push rax
-addr_21:
-	mov rax, [index]
-	push rax
-addr_22:
-	pop rax
-	pop rbx
-	add rax, rbx
-        push rax
-addr_23:
-	pop rax
-	mov [result], rax
-endif_addr_0:
-addr_24:
-	mov rax, [index]
-	push rax
-addr_25:
-	mov rax, 1
-	push rax
-addr_26:
-	pop rax
-	pop rbx
-	add rax, rbx
-        push rax
-addr_27:
-	pop rax
-	mov [index], rax
-	jmp loopaddr_0
-endloop_addr_0:
-addr_28:
-	mov rax, [result]
-	push rax
-addr_29:
-	pop rdi
-	call dump
-addr_30:
-	mov rax, 0
-	push rax
-addr_31:
-	pop rax
 	ret
-addr_32:
+addr_11:
 	mov rax, 0
 	ret
 block_addr_0:
-addr_33:
+addr_12:
 	mov rdi, rax
 	mov rax, 60
 	syscall
 segment readable writeable
-index dq 0
-result dq 0
 call_flag db 0
 cond_flag db 0
---
+++ test.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from pathlib import Path
+import subprocess
+
+dir_name = "tests"
+directory = Path(dir_name)
+
+def compare_results(path, output):
+    with open(f"{dir_name}/{path}", "r") as file:
+        content = file.read()
+    return (output == content), content
+
+for file in directory.iterdir():
+    if file.is_file():
+        if not file.name.endswith(".fox"):
+            continue
+
+        subprocess.run(["./build/foxc", f"{dir_name}/{file.name}"])
+        execution = subprocess.run("./output", capture_output=True, text=True, shell=True)
+
+        result = compare_results(file.name.replace(".fox", ".txt"), execution.stdout)
+        if not result[0]:
+            print(f"\nResult for: '{file.name}' is incorrect")
+            print("======================")
+            print(f"Result: {execution.stdout}")
+            print("======================")
+            print(f"Expected: {result[1]}")
