03e25009fd712792c64e25ed3ac5aa0e2a6e0929
stonedeva <leonardstonehoff@gmail.com>
2024-10-25 13:01:07 +0200

    Correct examples & euler problem 002

--- output.asm
+++ output.asm
@@ -37,30 +37,149 @@ main:
 	mov byte [call_flag], 0
 	jne block_addr_0
 addr_1:
-	mov rax, 250
+	mov rax, 1
 	push rax
-	pop [x]
+	pop [a]
 addr_2:
-	mov rax, x
+	mov rax, 2
 	push rax
+	pop [b]
 addr_3:
-	pop rdi
-	call dump
+	mov rax, 0
+	push rax
+	pop [sum]
 addr_4:
 	mov rax, 0
 	push rax
-addr_5:
-	pop rax
-	ret
+	pop [next_fib]
+loopaddr_0:
 addr_6:
+	mov rax, [a]
+	push rax
+addr_7:
+	mov rax, 4000000
+	push rax
+addr_8:
+	pop rax
+	pop rbx
+	cmp rax, rbx
+	sete al
+	movzx rax, al
+        push rax
+addr_9:
+	mov rax, [a]
+	push rax
+addr_10:
+	mov rax, 4000000
+	push rax
+addr_11:
+	pop rax
+	pop rbx
+	cmp rax, rbx
+	setg al
+	movzx rax, al
+        push rax
+addr_12:
+	pop rax
+	pop rbx
+	or rax, rbx
+        push rax
+addr_13:
+	pop rax
+	mov [cond_flag], al
+	cmp rax, 1
+	je addr_14
+	jne endloop_addr_0
+addr_14:
+	mov rax, 2
+	push rax
+addr_15:
+	mov rax, [a]
+	push rax
+addr_16:
+	pop rax
+	pop rbx
+	xor rdx, rdx
+	div rbx
+	mov rax, rdx
+        push rax
+addr_17:
+	mov rax, 0
+	push rax
+addr_18:
+	pop rax
+	pop rbx
+	cmp rax, rbx
+	sete al
+	movzx rax, al
+        push rax
+addr_19:
+	pop rax
+	mov [cond_flag], al
+	cmp rax, 1
+	je addr_20
+	jne endif_addr_0
+addr_20:
+	mov rax, [sum]
+	push rax
+addr_21:
+	mov rax, [a]
+	push rax
+addr_22:
+	pop rax
+	pop rbx
+	add rax, rbx
+        push rax
+addr_23:
+	pop rax
+	mov [sum], rax
+endif_addr_0:
+addr_24:
+	mov rax, [a]
+	push rax
+addr_25:
+	mov rax, [b]
+	push rax
+addr_26:
+	pop rax
+	pop rbx
+	add rax, rbx
+        push rax
+addr_27:
+	pop rax
+	mov [next_fib], rax
+addr_28:
+	mov rax, [b]
+	push rax
+addr_29:
+	pop rax
+	mov [a], rax
+addr_30:
+	mov rax, [next_fib]
+	push rax
+addr_31:
+	pop rax
+	mov [b], rax
+	jmp loopaddr_0
+endloop_addr_0:
+addr_32:
+	mov rax, [sum]
+	push rax
+addr_33:
+	pop rdi
+	call dump
+addr_34:
 	mov rax, 0
 	ret
 block_addr_0:
-addr_7:
+addr_35:
 	mov rdi, rax
 	mov rax, 60
 	syscall
 segment readable writeable
-x dq 0
+a dq 0
+b dq 0
+sum dq 0
+next_fib dq 0
 call_flag db 0
 cond_flag db 0
